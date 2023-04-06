09bd8f937dec4f78f05130a8e96ede0ae785456e
Lucas Sousa <lucas996oliveira@gmail.com>
2023-04-06 01:24:05 -0300

    Remove package import, fix error to compile

--- Manipulacao_de_Arquivos/FileReader_BufferReader/Main.java
+++ Manipulacao_de_Arquivos/FileReader_BufferReader/Main.java
@@ -1,5 +1,20 @@
-package FileReader_BufferReader;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        String path = "/tmp/java_test.txt";
+       
+        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line = br.readLine();
+            while(line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } 
+        catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } 
+    }
 }
