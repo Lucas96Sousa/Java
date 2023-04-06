a21cefe55e3d934af73b5551d2d05bd9aae6e23c
Lucas Sousa <lucas996oliveira@gmail.com>
2023-04-06 02:59:43 -0300

    Solução parte 1

---
+++ Interfaces/Readme.md
@@ -0,0 +1,20 @@
+## Interfaces
+
+[Módulo de apresentação das interfaces  e herança multipla.]
+
+# Referencia sobre interface
+
+<p>Interface é um tipo que define um conjuto de operações que uma classe deve implementar.</p>
+
+<p> A interface estabelece um <b>contrato</b> que a classe deve cumprir.<p>
+
+```java
+interface Shape {
+    double area();
+    double perimeter();
+}
+```
+
+<strong> Pra quê interfaces ?</strong>
+
+* Para criar sistemas com baixo acoplamento e flexíveis.
---
+++ Interfaces/withoutInterface/Main.java
@@ -0,0 +1,23 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+         
+        System.out.println("Entre com os dados do aluguel: ");
+        System.out.print("Modelo do carro: ");
+        String carModel = sc.nextLine();
+        System.out.println("Retirada (dd/MM/yyyy HH:mm): ");
+        LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+        System.out.println();
+
+
+        sc.close();
+    }
+}
