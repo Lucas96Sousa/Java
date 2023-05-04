98b219c1dca118805d73aaf51e8e9ba167e5cdb4
Lucas <lucas996oliveira@gmail.com>
2023-05-03 22:37:36 -0300

    Create generic methods

--- Generics_Set_Map/Generic_module/src/App.java
+++ Generics_Set_Map/Generic_module/src/App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
+import service.PrintService;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Scanner sc = new Scanner(System.in);
+
+       PrintService ps = new PrintService();
+
+       System.out.print("How many values? ");
+        int n = sc.nextInt();
+
+        for (int i=0; i < n; i++) {
+            int value = sc.nextInt();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        System.out.print("First: " + ps.first());
+
+        sc.close();
     }
 }
