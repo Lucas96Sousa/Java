de0767070bcae79b0db19bc5932df169a5c0f71a
Lucas Sousa <lucas996oliveira@gmail.com>
2023-05-31 20:55:45 -0300

    Create hashCode and Equal method and Set

--- Generics_Set_Map/Generic_delimitation/src/App.java
+++ Generics_Set_Map/Generic_delimitation/src/App.java
@@ -1,33 +1,32 @@
+import entities.Product;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-import entities.Product;
 import service.CalculationService;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-       List<Product> list = new ArrayList<>();
+  public static void main(String[] args) throws Exception {
+    List<Product> list = new ArrayList<>();
 
-       String path = "/tmp/in.txt";
+    String path = "/tmp/in.txt";
 
-       try(BufferedReader br = new BufferedReader(new FileReader(path))){
+    try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
-        String line = br.readLine();
-        while(line != null) {
-            String[] fields = line.split(",");
-            list.add(new Product (fields[0], Double.parseDouble(fields[1])));
-            line = br.readLine();
-        }
+      String line = br.readLine();
+      while (line != null) {
+        String[] fields = line.split(",");
+        list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+        line = br.readLine();
+      }
 
-        Product x = CalculationService.max(list);
-        System.out.println("Most expensive: ");
-        System.out.println(x);
+      Product x = CalculationService.max(list);
+      System.out.println("Most expensive: ");
+      System.out.println(x);
 
-       }catch(IOException e){
-        System.out.println("Error: " + e.getMessage());
-       }
+    } catch (IOException e) {
+      System.out.println("Error: " + e.getMessage());
     }
+  }
 }
