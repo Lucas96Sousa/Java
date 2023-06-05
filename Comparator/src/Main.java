3e60ac2e0ae2dd2d79e1ff892f21672817769cd2
Lucas Sousa <lucas996oliveira@gmail.com>
2023-06-04 21:08:44 -0300

    Comparator

---
+++ Comparator/src/Main.java
@@ -0,0 +1,20 @@
+import entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.000));
+        list.add(new Product("Laptop", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        list.sort(new MyComparator());
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+    }
+}
