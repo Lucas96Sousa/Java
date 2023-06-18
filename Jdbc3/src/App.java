f4cf9980f157efa93fbec946571872c8bf9e03e4
Lucas Sousa <lucas996oliveira@gmail.com>
2023-06-18 19:14:02 -0300

    Create method to insert data

---
+++ Jdbc3/src/App.java
@@ -0,0 +1,53 @@
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+
+import db.DB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try {
+            conn = DB.getConnection();
+            st = conn.prepareStatement(
+                    "insert into seller " +
+                            "(Name, Email, BirthDate, BaseSalary, DepartmentId) " +
+                            "VALUES " +
+                            "(?, ?, ?, ?, ?)", 
+                            Statement.RETURN_GENERATED_KEYS);
+
+            // Bloco de comando sql
+            st.setString(1, "Carl Purple");
+            st.setString(2,"carl@gmail.com");
+            st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+            st.setDouble(4, 3000.00);
+            st.setInt(5, 4);
+
+            int rowsAffected = st.executeUpdate();
+
+            if(rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                while(rs.next()){
+                    int id = rs.getInt(1);
+                    System.out.println("Done! ID = " + id);
+                }
+            }else {
+                System.out.println("No rows affected");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
+    }
+}
