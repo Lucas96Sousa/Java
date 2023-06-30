4733c753c99c605008f3c15ce6c5cf0e20e506b7
Lucas Sousa <lucas996oliveira@gmail.com>
2023-06-30 20:26:43 -0300

    Done update table.

---
+++ Jdbc4/src/App.java
@@ -0,0 +1,37 @@
+import db.DB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try {
+            conn = DB.getConnection();
+            st = conn.prepareStatement(
+                    "UPDATE seller " +
+                    "SET BaseSalary =  BaseSalary + ? " +
+                    "WHERE " +
+                    "(DepartmentId = ?)"
+            );
+            // Bloco de comando sql
+            st.setDouble(1, 200.0);
+            st.setInt(2, 2);
+            
+            int rowsAffected = st.executeUpdate();
+
+           System.out.println("Done ! Row affected : " + rowsAffected);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
+    }
+}
