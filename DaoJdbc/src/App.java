9e169d34052b0eab9c5144370c47dc66cefcc026
Lucas Sousa <lucas996oliveira@gmail.com>
2023-06-30 21:27:33 -0300

    Refatorando para receber os dados do DAO

--- DaoJdbc/src/App.java
+++ DaoJdbc/src/App.java
@@ -5,7 +5,6 @@ import db.DB;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class App {
@@ -16,31 +15,8 @@ public class App {
 
         try {
             conn = DB.getConnection();
-            st = conn.prepareStatement(
-                    "insert into seller " +
-                            "(Name, Email, BirthDate, BaseSalary, DepartmentId) " +
-                            "VALUES " +
-                            "(?, ?, ?, ?, ?)", 
-                            Statement.RETURN_GENERATED_KEYS);
-
-            // Bloco de comando sql
-            st.setString(1, "Carl Purple");
-            st.setString(2,"carl@gmail.com");
-            st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-            st.setDouble(4, 3000.00);
-            st.setInt(5, 4);
 
             int rowsAffected = st.executeUpdate();
-
-            if(rowsAffected > 0) {
-                ResultSet rs = st.getGeneratedKeys();
-                while(rs.next()){
-                    int id = rs.getInt(1);
-                    System.out.println("Done! ID = " + id);
-                }
-            }else {
-                System.out.println("No rows affected");
-            }
 
         } catch (SQLException e) {
             e.printStackTrace();
---
+++ DaoJdbc/src/model/entities/Department.java
@@ -0,0 +1,38 @@
+package model.entities;
+import java.util.Objects;
+
+public class Department {
+
+    private Integer id;
+    private String name;
+
+
+    public Department() {
+    }
+
+
+    public Department(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+    
+}
