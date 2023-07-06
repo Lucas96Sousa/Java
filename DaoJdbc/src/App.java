cf8fb07cd24ab29a04ac4c5c8942c0afc0971eb1
Lucas Sousa <lucas996oliveira@gmail.com>
2023-07-06 19:31:40 -0300

    Create SellerDao and DaoFactory

--- DaoJdbc/src/App.java
+++ DaoJdbc/src/App.java
@@ -1,18 +1,20 @@
 import java.util.Date;
-
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
+
 
 public class App {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "books");
         System.out.println(obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 2000.0, obj);
         System.out.println(seller);
-
-
         
     }
 }
