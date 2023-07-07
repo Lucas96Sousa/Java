7fd68a4ac39c525f0646917e445138fe778e513e
Lucas Sousa <lucas996oliveira@gmail.com>
2023-07-07 06:00:06 -0300

    Create method: FindByDepartment

--- DaoJdbc/src/App.java
+++ DaoJdbc/src/App.java
@@ -1,20 +1,25 @@
 import java.sql.SQLException;
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
-
-
 
 public class App {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        
 
         System.out.println("=== TEST 01: SELLER FINDBY ID ====");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
-        
+
+        System.out.println("\n== TESTE02 SELLER findByDepartment ====");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for(Seller obj : list) {
+            System.out.println(obj);
+        }
     }
 }
