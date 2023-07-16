a9dcd1ced043ac104d81c402b4bac2b2e40c3961
Lucas Sousa <lucas996oliveira@gmail.com>
2023-07-16 01:24:59 -0300

    Need fix db connection

---
+++ JPAMAVEN/jpamaven/src/main/java/com/jpaweb/App.java
@@ -0,0 +1,27 @@
+package com.jpaweb;
+
+
+import com.jpaweb.domain.Person;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Person p1 = new Person(null, "Carlos da Silva", "carlos@gmail.com");
+        Person p2 = new Person(null, "Joaquim Phoenix", "joaquim@gmail.com");
+        Person p3 = new Person(null, "Javier Barden", "javier@gmail.com");
+
+        EntityManagerFactory emf =  Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = (EntityManager) emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+        em.getTransaction().commit();
+        System.out.println("Finish");
+    }
+}
