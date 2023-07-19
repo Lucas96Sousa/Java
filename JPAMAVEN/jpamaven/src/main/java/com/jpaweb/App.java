d856580f60bb9e0f6a8bf5cc8ab61c454a2e3f7c
Lucas Sousa <lucas996oliveira@gmail.com>
2023-07-18 22:46:08 -0300

    Finally JPAMAVEN

--- JPAMAVEN/jpamaven/src/main/java/com/jpaweb/App.java
+++ JPAMAVEN/jpamaven/src/main/java/com/jpaweb/App.java
@@ -9,19 +9,17 @@ import jakarta.persistence.Persistence;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Person p1 = new Person(null, "Carlos da Silva", "carlos@gmail.com");
-        Person p2 = new Person(null, "Joaquim Phoenix", "joaquim@gmail.com");
-        Person p3 = new Person(null, "Javier Barden", "javier@gmail.com");
+
 
         EntityManagerFactory emf ;
         emf = Persistence.createEntityManagerFactory("jpa");
         EntityManager em = (EntityManager) emf.createEntityManager();
 
-        em.getTransaction().begin();
-        em.persist(p1);
-        em.persist(p2);
-        em.persist(p3);
-        em.getTransaction().commit();
+        Person p = em.find(Person.class, 2);
+        System.out.println(p);
+
         System.out.println("Finish");
+        em.close();
+        emf.close();
     }
 }
