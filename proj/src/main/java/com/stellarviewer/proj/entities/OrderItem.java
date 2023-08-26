4e355f5127750308262f632e74fbaa7de22499bd
Lucas Sousa <lucas996oliveira@gmail.com>
2023-08-25 22:47:24 -0300

    Feature: create OrderItems Seed data

--- proj/src/main/java/com/stellarviewer/proj/entities/OrderItem.java
+++ proj/src/main/java/com/stellarviewer/proj/entities/OrderItem.java
@@ -9,81 +9,89 @@ import jakarta.persistence.Entity;
 
 @Entity(name = "order_item")
 public class OrderItem implements Serializable {
-    
-    @EmbeddedId
-    private OrderItemPK id;
 
-    
-    private Integer quantity;
-    private Double price;
+  @EmbeddedId
+  private OrderItemPK id = new OrderItemPK();
 
-    public OrderItem() {
+  private Integer quantity;
+  private Double price;
 
-    }
 
-    public OrderItem(Order order, Product product, Integer quantity, Double price) {
-        id.setOrder(order);
-        id.setProduct(product);
-        this.quantity = quantity;
-        this.price = price;
-    }
 
-    public Order getOrder() {
-        return id.getOrder();
-    }
+  public OrderItem() {
 
-    public void setOrder(Order order) {
-        id.setOrder(order);
-    }
-    
-    public Product getProduct() {
-        return id.getProduct();
-    }
+  }
 
-    public void setProduct(Product product) {
-        id.setProduct(product);
-    }
+  public OrderItem(Order order, Product product, Integer quantity, Double price) {
+    id.setOrder(order);
+    id.setProduct(product);
+    this.quantity = quantity;
+    this.price = price;
+  }
 
-    public Integer getQuantity() {
-        return quantity;
-    }
+  public Order getOrder() {
+    return id.getOrder();
+  }
 
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
+  public void setOrder(Order order) {
+    id.setOrder(order);
+  }
 
-    public Double getPrice() {
-        return price;
-    }
+  public Product getProduct() {
+    return id.getProduct();
+  }
 
-    public void setPrice(Double price) {
-        this.price = price;
-    }
+  public void setProduct(Product product) {
+    id.setProduct(product);
+  }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
+  public Integer getQuantity() {
+    return quantity;
+  }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        OrderItem other = (OrderItem) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
+  public void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+  }
 
+  public Double getPrice() {
+    return price;
+  }
+
+  public void setPrice(Double price) {
+    this.price = price;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    OrderItem other = (OrderItem) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
+  public OrderItemPK getId() {
+    return id;
+  }
+
+  public void setId(OrderItemPK id) {
+    this.id = id;
+  }
 
 }
