c3cb1fbd239590fc230b26dbc4d71fe96aa77164
Lucas Sousa <Lucas996oliveira@gmail.com>
2023-09-03 21:09:57 -0300

    feat: create update method url

--- proj/src/main/java/com/stellarviewer/proj/resources/UserResource.java
+++ proj/src/main/java/com/stellarviewer/proj/resources/UserResource.java
@@ -1,8 +1,5 @@
 package com.stellarviewer.proj.resources;
 
-import com.stellarviewer.proj.entities.User;
-import com.stellarviewer.proj.services.UserService;
-
 import java.net.URI;
 import java.util.List;
 
@@ -12,10 +9,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.stellarviewer.proj.entities.User;
+import com.stellarviewer.proj.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -49,4 +50,12 @@ public class UserResource {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        obj = service.update(id, obj);
+
+        return ResponseEntity.ok().body(obj);
+    }
+
 }
