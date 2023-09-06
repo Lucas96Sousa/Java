4ad95665912c18ce49cab0de29417f771e4c60f9
Lucas Sousa <Lucas996oliveira@gmail.com>
2023-09-06 19:41:37 -0300

    feat: create delete ErrorHandler

--- proj/src/main/java/com/stellarviewer/proj/services/UserService.java
+++ proj/src/main/java/com/stellarviewer/proj/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.stellarviewer.proj.entities.User;
 import com.stellarviewer.proj.repositories.UserRepository;
+import com.stellarviewer.proj.services.exceptions.DatabaseException;
 import com.stellarviewer.proj.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -20,18 +23,25 @@ public class UserService {
         return repository.findAll();
     }
 
-	public User findById(Long id) {
-		Optional<User> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
-	}
-    
-    public User insert(User obj) {
-        return repository.save(obj);
-            
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
-    public void delete(Long id){
-        repository.deleteById(id);
+    public User insert(User obj) {
+        return repository.save(obj);
+
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        }catch(DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+
     }
 
     public User update(Long id, User obj) {
@@ -45,5 +55,5 @@ public class UserService {
         entity.setEmail(obj.getEmail());
         entity.setPhone(obj.getPhone());
     }
- 
+
 }
